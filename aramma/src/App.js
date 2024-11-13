2d0ae8ce02979f7bf83df9dbc237d3aea4c632fa
Nasirgrema <muhammadnasirg250@gmail.com>
2024-11-13 06:40:54 -0800

    feat: modifications

---
+++ aramma/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="demo">
+      welcome to arammas react App
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='App'>
+        <h1>welcome to react</h1>
+      </div>
+    </>
+  );
+}
+
+export default App
